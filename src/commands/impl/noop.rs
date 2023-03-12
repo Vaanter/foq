855d1c5a33bea48e07dcb8280830a8ad38fa6870
Valentin Bolfik <valentin.bolfik@gmail.com>
2023-03-12 21:43:59 +0100

    Add noop command

--- src/commands/impl/noop.rs
+++ src/commands/impl/noop.rs
@@ -3,6 +3,8 @@ use async_trait::async_trait;
 use crate::commands::command::Command;
 use crate::commands::executable::Executable;
 use crate::handlers::reply_sender::ReplySend;
+use crate::io::reply::Reply;
+use crate::io::reply_code::ReplyCode;
 use crate::io::session::Session;
 
 pub(crate) struct Noop;
@@ -10,6 +12,8 @@ pub(crate) struct Noop;
 #[async_trait]
 impl Executable for Noop {
   async fn execute(session: &mut Session, command: &Command, reply_sender: &mut impl ReplySend) {
-    todo!()
+    reply_sender
+      .send_control_message(Reply::new(ReplyCode::CommandOkay, "OK"))
+      .await;
   }
 }
