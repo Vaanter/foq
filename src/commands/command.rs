31fb23bb580a8ba3058c0a02ac057a0fe0bf994c
Valentín Bolfík <valentin.bolfik@student.tuke.sk>
2023-04-09 00:56:27 +0200

    Reformat

--- src/commands/command.rs
+++ src/commands/command.rs
@@ -1,25 +1,30 @@
-use crate::commands::commands::Commands;
 use anyhow::anyhow;
 
+use crate::commands::commands::Commands;
+
 pub(crate) struct Command {
-    pub(crate) command: Commands,
-    pub(crate) argument: String,
+  pub(crate) command: Commands,
+  pub(crate) argument: String,
 }
 
 impl Command {
-    pub(crate) fn new(command: Commands, argument: impl Into<String>) -> Self {
-        return Command { command, argument: argument.into() };
-    }
+  pub(crate) fn new(command: Commands, argument: impl Into<String>) -> Self {
+    return Command {
+      command,
+      argument: argument.into(),
+    };
+  }
 
-    pub(crate) fn parse(message: &str) -> Result<Self, anyhow::Error> {
-        let mut split = message.split(" ");
-        let command = split.next().ok_or_else(|| anyhow!("Invalid command!"))?;
-        let argument = split.next().unwrap_or("");
-        Ok(Command {
-            command: command.parse()?,
-            argument: argument.to_owned(),
-        })
-    }
+  pub(crate) fn parse(message: &str) -> Result<Self, anyhow::Error> {
+    let mut split = message.split(" ");
+    let command = split.next().ok_or_else(|| anyhow!("Invalid command!"))?;
+    let argument = split.next().unwrap_or("");
+    Ok(Command {
+      command: command.parse()?,
+      argument: argument.to_owned(),
+    })
+  }
+}
 
 #[cfg(test)]
 mod tests {
