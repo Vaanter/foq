a75a98b7cfa825699ae8b21d2fabd20e4e5085b1
Valentín Bolfík <valentin.bolfik@student.tuke.sk>
2023-05-05 00:12:38 +0200

    Load certificate specified in config

--- src/global_context.rs
+++ src/global_context.rs
@@ -1,12 +1,50 @@
+use std::path::Path;
+
+use config::Config;
 use once_cell::sync::Lazy;
+use rustls::{Certificate, PrivateKey};
 use sqlx::SqlitePool;
 use tokio::sync::OnceCell;
 use tracing::info;
 
 use crate::auth::auth_provider::AuthProvider;
+use crate::utils::tls_utils::{load_certs, load_keys};
+
+pub(crate) static CONFIG: Lazy<Config> = Lazy::new(|| {
+  Config::builder()
+    .add_source(config::File::with_name("config.toml"))
+    // Add in settings from the environment (with a prefix of FOQ)
+    // Eg.. `FOQ_DEBUG=1 ./target/app` would set the `debug` key
+    .add_source(config::Environment::with_prefix("FOQ"))
+    .build()
+    .unwrap()
+});
+
+pub(crate) static CERTS: Lazy<Vec<Certificate>> = Lazy::new(|| {
+  load_certs(Path::new(
+    &CONFIG
+      .get_string("certificate_file")
+      .expect("Certificate must be supplied in config!"),
+  ))
+  .expect("Unable to load certificates! Cannot start.")
+});
+
+pub(crate) static KEY: Lazy<PrivateKey> = Lazy::new(|| {
+  load_keys(Path::new(
+    &CONFIG
+      .get_string("key_file")
+      .expect("Key must be supplied in config!"),
+  ))
+  .expect("Unable to load keys! Cannot start.")
+  .get(0)
+  .unwrap()
+  .clone()
+});
 
 pub(crate) static DB_LAZY: Lazy<SqlitePool> = Lazy::new(|| {
-  let db_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set!");
+  let db_url = CONFIG
+    .get_string("DATABASE_URL")
+    .expect("DATABASE_URL must be set!");
   info!("DATABASE_URL: {db_url}");
   SqlitePool::connect_lazy(&db_url).unwrap()
 });
--- src/listeners/quic_only_listener.rs
+++ src/listeners/quic_only_listener.rs
@@ -1,41 +1,42 @@
 use std::error::Error;
 use std::net::SocketAddr;
-use std::sync::Arc;
 
-use s2n_quic::{provider::io::tokio::Builder as IoBuilder, Connection, Server};
-use tokio::sync::Mutex;
+use rustls::ServerConfig;
+use s2n_quic::{
+  provider::io::tokio::Builder as IoBuilder, provider::tls::default::Server as TlsServer,
+  Connection, Server,
+};
 use tokio_util::sync::CancellationToken;
 use tracing::info;
 
-use crate::handlers::quic_only_connection_handler::QuicOnlyConnectionHandler;
-
-/// NOTE: this certificate is to be used for demonstration purposes only!
-pub static CERT_PEM: &str = include_str!(concat!(
-  env!("CARGO_MANIFEST_DIR"),
-  "/certs/server-cert.pem"
-));
-/// NOTE: this certificate is to be used for demonstration purposes only!
-pub static KEY_PEM: &str =
-  include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/certs/server-key.pem"));
+use crate::global_context::{CERTS, KEY};
 
 pub(crate) struct QuicOnlyListener {
   pub(crate) server: Server,
-  pub(crate) handlers: Vec<Arc<Mutex<QuicOnlyConnectionHandler>>>,
 }
 
 impl QuicOnlyListener {
   pub(crate) fn new(addr: SocketAddr) -> Result<Self, Box<dyn Error>> {
     let io = IoBuilder::default().with_receive_address(addr)?.build()?;
 
+    let certs = CERTS.clone();
+    let key = KEY.clone();
+
+    let mut config = ServerConfig::builder()
+      .with_safe_defaults()
+      .with_no_client_auth()
+      .with_single_cert(certs, key)
+      .map_err(|err| tokio::io::Error::new(tokio::io::ErrorKind::InvalidInput, err))?;
+    config.alpn_protocols = vec!["foq".as_bytes().to_vec()];
+
+    let tls_server = TlsServer::new(config);
+
     let server = Server::builder()
-      .with_tls((CERT_PEM, KEY_PEM))?
+      .with_tls(tls_server)?
       .with_io(io)?
       .start()?;
 
-    Ok(QuicOnlyListener {
-      server,
-      handlers: vec![],
-    })
+    Ok(QuicOnlyListener { server })
   }
 
   #[tracing::instrument(skip(self))]
