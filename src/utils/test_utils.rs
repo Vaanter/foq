7144d9223895789de677609eee9adc939d24d3c0
Valentín Bolfík <valentin.bolfik@student.tuke.sk>
2023-05-05 23:02:55 +0200

    Add more helper functions

--- src/utils/test_utils.rs
+++ src/utils/test_utils.rs
@@ -7,18 +7,28 @@ use std::time::{Duration, SystemTime};
 use async_trait::async_trait;
 use rustls::client::{ServerCertVerified, ServerCertVerifier};
 use rustls::{Certificate, ClientConfig, ServerConfig, ServerName};
+use tokio::io::{
+  AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, BufReader, BufWriter,
+};
+use tokio::net::TcpStream;
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::mpsc::Sender;
+use tokio::task::JoinHandle;
 use tokio::time::timeout;
+use tokio_util::sync::CancellationToken;
 
 use crate::auth::auth_error::AuthError;
 use crate::auth::auth_provider::AuthProvider;
 use crate::auth::data_source::DataSource;
 use crate::auth::login_form::LoginForm;
 use crate::auth::user_data::UserData;
+use crate::handlers::connection_handler::ConnectionHandler;
+use crate::handlers::quic_only_connection_handler::QuicOnlyConnectionHandler;
 use crate::handlers::reply_sender::ReplySend;
+use crate::io::command_processor::CommandProcessor;
 use crate::io::reply::Reply;
 use crate::io::reply_code::ReplyCode;
+use crate::listeners::quic_only_listener::QuicOnlyListener;
 use crate::utils::tls_utils::{load_certs, load_keys};
 
 pub(crate) struct TestReplySender {
@@ -35,7 +45,7 @@ impl TestReplySender {
 impl ReplySend for TestReplySender {
   async fn send_control_message(&self, reply: Reply) {
     println!(
-      "TestReplySender: received reply: {}",
+      "[TestReplySender] Received reply: {}",
       reply.to_string().trim_end()
     );
     self.tx.send(reply).await.unwrap();
@@ -65,7 +75,7 @@ impl TestDataSource {
 #[async_trait]
 impl DataSource for TestDataSource {
   async fn authenticate(&self, login_form: &LoginForm) -> Result<UserData, AuthError> {
-    eprintln!("Received: {:?}", login_form);
+    eprintln!("[TestDataSource] Login attempt with: {:?}", login_form);
     let user = self
       .user_data
       .iter()
@@ -109,6 +119,35 @@ pub(crate) async fn receive_and_verify_reply(
   };
 }
 
+pub(crate) async fn receive_and_verify_reply_from_buf<T: AsyncRead + Unpin>(
+  time: u64,
+  client_reader: &mut BufReader<T>,
+  expected: ReplyCode,
+  substring: Option<&str>,
+) {
+  let mut buffer = String::new();
+  match timeout(
+    Duration::from_secs(time),
+    client_reader.read_line(&mut buffer),
+  )
+  .await
+  {
+    Ok(Ok(len)) => {
+      println!(
+        "Received reply from server!: {}. Length: {}",
+        buffer.trim(),
+        len
+      );
+      assert!(buffer.trim().starts_with(&(expected as u32).to_string()));
+      assert!(buffer.trim().contains(substring.unwrap_or("")));
+    }
+    Ok(Err(e)) => {
+      panic!("Failed to read reply! {}", e);
+    }
+    Err(_) => panic!("Timeout reading reply message!"),
+  }
+}
+
 pub(crate) struct NoCertificateVerification {}
 
 impl NoCertificateVerification {
@@ -132,7 +171,6 @@ impl ServerCertVerifier for NoCertificateVerification {
   }
 }
 
-#[cfg(test)]
 pub(crate) fn create_test_client_config() -> ClientConfig {
   ClientConfig::builder()
     .with_safe_defaults()
@@ -140,7 +178,6 @@ pub(crate) fn create_test_client_config() -> ClientConfig {
     .with_no_client_auth()
 }
 
-#[cfg(test)]
 pub(crate) fn create_test_server_config() -> ServerConfig {
   let cert = load_certs(Path::new(CERT_PATH)).unwrap();
   let mut key = load_keys(Path::new(KEY_PATH)).unwrap();
@@ -152,3 +189,63 @@ pub(crate) fn create_test_server_config() -> ServerConfig {
 }
 
 pub(crate) const LOCALHOST: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
+
+pub(crate) async fn open_tcp_data_channel(command_processor: &mut CommandProcessor) -> TcpStream {
+  let addr = match command_processor
+    .data_wrapper
+    .clone()
+    .lock()
+    .await
+    .open_data_stream()
+    .await
+  {
+    Ok(addr) => addr,
+    Err(_) => panic!("Failed to open passive data listener!"),
+  };
+
+  println!("Connecting to passive listener");
+  let client_dc = match TcpStream::connect(addr).await {
+    Ok(c) => c,
+    Err(e) => {
+      panic!("Client passive connection failed: {}", e);
+    }
+  };
+  println!("Client passive connection successful!");
+
+  let _ = command_processor
+    .data_wrapper
+    .lock()
+    .await
+    .get_data_stream()
+    .await
+    .lock()
+    .await;
+  client_dc
+}
+
+pub(crate) async fn run_quic_listener(
+  ct: CancellationToken,
+  server_addr: SocketAddr,
+) -> (JoinHandle<()>, SocketAddr) {
+  let mut listener = QuicOnlyListener::new(server_addr).unwrap();
+  let addr = listener.server.local_addr().unwrap();
+  let handler_fut = tokio::spawn(async move {
+    let conn = listener.accept(ct.clone()).await.unwrap();
+    let mut handler = QuicOnlyConnectionHandler::new(conn);
+
+    handler
+      .handle(ct)
+      .await
+      .expect("Handler should exit gracefully");
+  });
+  (handler_fut, addr)
+}
+
+pub(crate) fn create_tls_client_config() -> ClientConfig {
+  let mut client_config = ClientConfig::builder()
+    .with_safe_defaults()
+    .with_custom_certificate_verifier(Arc::new(NoCertificateVerification::new()))
+    .with_no_client_auth();
+  client_config.alpn_protocols = vec!["foq".as_bytes().to_vec()];
+  client_config
+}
