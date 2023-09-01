69bec20fc18cccc0edf469fa71fc8761be8cfd7d
Valentín Bolfík <valentin.bolfik@student.tuke.sk>
2023-09-01 17:59:04 +0200

    Add Windows job

--- .space.kts
+++ .space.kts
@@ -4,12 +4,29 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Build, latest") {
+job("Build Linux on latest container") {
     container(displayName = "Run script", image = "rust:latest") {
         shellScript {
             content = """
                 set -e
                 # Build the Rust project
+                cargo build --verbose
+            """
+        }
+    }
+}
+
+job("Build Windows on host") {
+    host(displayName = "Windows") {
+        requirements {
+            os {
+                type = OSType.Windows
+                arch = "x86_64"
+            }
+        }
+
+        shellScript {
+            content = """
                 cargo build --verbose
             """
         }
