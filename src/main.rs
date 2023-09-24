841d84deb0221166bbd19fda8d61f7f08fc0a279
Valentín Bolfík <valentin.bolfik@student.tuke.sk>
2023-09-24 16:20:07 +0200

    Write traces to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::OpenOptions;
 use std::str::FromStr;
 
 use tracing::Level;
@@ -29,14 +30,26 @@ mod utils;
 ///
 #[tokio::main]
 async fn main() {
+  let mut log_file_options = OpenOptions::new();
+  log_file_options
+    .write(true)
+    .append(true)
+    .truncate(false)
+    .create(true);
+  let log_file = log_file_options
+    .open("foq.log")
+    .expect("Log file should be accessible");
+  let (non_blocking, _guard) = tracing_appender::non_blocking(log_file);
   let log_level = Level::from_str(&CONFIG.get_string("log_level").unwrap_or(String::new()))
     .unwrap_or(Level::INFO);
   let subscriber = tracing_subscriber::fmt()
+    .with_writer(non_blocking)
+    .with_env_filter(format!("foq={}", log_level))
     .with_file(false)
+    .with_ansi(false)
     .with_line_number(false)
     .with_thread_ids(true)
     .with_target(false)
-    .with_max_level(log_level)
     .finish();
   tracing::subscriber::set_global_default(subscriber).unwrap();
 
