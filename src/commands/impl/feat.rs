c88c30b38c4350dca7daadeced9fe9b9c13c8389
Valentín Bolfík <valentin.bolfik@student.tuke.sk>
2023-09-24 13:18:42 +0200

    Refactor FEAT

--- src/commands/impl/feat.rs
+++ src/commands/impl/feat.rs
@@ -1,23 +1,26 @@
+use std::iter::Iterator;
+
 use async_trait::async_trait;
+use once_cell::sync::Lazy;
 
 use crate::commands::command::Command;
 use crate::commands::commands::Commands;
 use crate::commands::executable::Executable;
-use crate::handlers::reply_sender::ReplySend;
-use crate::session::command_processor::CommandProcessor;
 use crate::commands::reply::Reply;
 use crate::commands::reply_code::ReplyCode;
+use crate::handlers::reply_sender::ReplySend;
+use crate::session::command_processor::CommandProcessor;
 
 #[derive(Copy, Clone, Eq, PartialEq, Default)]
 pub(crate) struct Feat;
 
-const FEATURES: [&str; 2] = ["MLSD", "UTF8"];
-
-impl Feat {
-  fn format_features() -> Vec<String> {
-    FEATURES.iter().map(|f| format!(" {}", f)).collect()
-  }
-}
+static LINES: Lazy<Vec<String>> = Lazy::new(|| {
+  let mut lines: Vec<String> = vec!["Supported features:".to_string()];
+  let features = vec!["MLSD", "REST STREAM", "UTF8"];
+  lines.extend(features.iter().map(|f| format!(" {}", f)));
+  lines.push("END".to_string());
+  lines
+});
 
 #[async_trait]
 impl Executable for Feat {
@@ -27,11 +30,9 @@ impl Executable for Feat {
     reply_sender: &mut impl ReplySend,
   ) {
     debug_assert_eq!(command.command, Commands::FEAT);
-    let mut lines: Vec<String> = vec!["Features supported: ".to_string()];
-    lines.append(&mut Feat::format_features());
-    lines.push("END".to_string());
+
     Feat::reply(
-      Reply::new_multiline(ReplyCode::SystemStatus, lines),
+      Reply::new_multiline(ReplyCode::SystemStatus, LINES.clone()),
       reply_sender,
     )
     .await;
@@ -48,7 +49,7 @@ mod tests {
   use crate::commands::command::Command;
   use crate::commands::commands::Commands;
   use crate::commands::executable::Executable;
-  use crate::commands::r#impl::feat::Feat;
+  use crate::commands::r#impl::feat::{Feat, LINES};
   use crate::data_channels::standard_data_channel_wrapper::StandardDataChannelWrapper;
   use crate::session::command_processor::CommandProcessor;
   use crate::session::session_properties::SessionProperties;
@@ -56,13 +57,14 @@ mod tests {
 
   #[tokio::test]
   async fn format_test() {
-    const EXPECTED: &str = " MLSD UTF8";
-    assert_eq!(EXPECTED, Feat::format_features().join(""))
+    assert_eq!(LINES.first().unwrap(), "Supported features:");
+    assert_eq!(LINES.last().unwrap(), "END");
   }
 
   #[tokio::test]
-  async fn full_reply() {
-    const EXPECTED: &str = "211-Features supported: \r\n MLSD\r\n UTF8\r\n211 END\r\n";
+  async fn full_reply_test() {
+    const EXPECTED: &str =
+      "211-Supported features:\r\n MLSD\r\n REST STREAM\r\n UTF8\r\n211 END\r\n";
     let session_properties = Arc::new(RwLock::new(SessionProperties::new()));
     let mut session = CommandProcessor::new(
       session_properties.clone(),
