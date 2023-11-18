9dae455d2c0ec7e8d76fb132815dc64b292bdf3d
Valentín Bolfík <valentin.bolfik@gmail.com>
2023-11-18 22:15:46 +0100

    Change FMT to a layer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::fs::OpenOptions;
 use std::str::FromStr;
 
 use tracing::Level;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Layer, Registry};
 
 use crate::global_context::CONFIG;
 
@@ -31,26 +34,23 @@ mod utils;
 #[tokio::main]
 async fn main() {
   let mut log_file_options = OpenOptions::new();
-  log_file_options
-    .write(true)
-    .truncate(true)
-    .create(true);
+  log_file_options.write(true).truncate(true).create(true);
   let log_file = log_file_options
     .open("foq.log")
     .expect("Log file should be accessible");
   let (non_blocking, _guard) = tracing_appender::non_blocking(log_file);
   let log_level =
     Level::from_str(&CONFIG.get_string("log_level").unwrap_or_default()).unwrap_or(Level::INFO);
-  let subscriber = tracing_subscriber::fmt()
+  let fmt_layer = tracing_subscriber::fmt::Layer::default()
     .with_writer(non_blocking)
-    .with_env_filter(format!("foq={}", log_level))
     .with_file(false)
     .with_ansi(false)
     .with_line_number(false)
     .with_thread_ids(true)
     .with_target(false)
-    .finish();
-  tracing::subscriber::set_global_default(subscriber).unwrap();
+    .with_filter(EnvFilter::new(format!("foq={}", log_level)));
+
+  Registry::default().with(fmt_layer).init();
 
   runner::run().await;
 }
